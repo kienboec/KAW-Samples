68a5a5db43278178b9a3e2b83859284e4132aa1c
kienboeck <kienboec@technikum-wien.at>
2021-03-04 12:29:45 +0100

    student grading system (preparation of challenge 1 in group 1)

---
+++ KAW1StudentGrading/KAW1StudentGrading/CodeGenerator.cs
@@ -0,0 +1,45 @@
+﻿
+
+
+
+
+namespace KAW1StudentGrading.StudentComponent {
+
+    public class Student {
+        public string Name { get; set; }
+    }
+}
+
+
+
+
+namespace KAW1StudentGrading.StudentComponent {
+
+    public class Laptop {
+        public string Name { get; set; }
+    }
+}
+
+
+
+
+namespace KAW1StudentGrading.LectureComponent {
+
+    public class Lecture {
+        public string Name { get; set; }
+    }
+}
+
+
+
+
+namespace KAW1StudentGrading.LectureComponent {
+
+    public class LectureGrade {
+        public string Name { get; set; }
+    }
+}
+
+
+
+
---
+++ KAW1StudentGrading/KAW1StudentGrading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace KAW1StudentGrading
+{
+    using KAW1StudentGrading.LectureComponent;
+    using KAW1StudentGrading.StudentComponent;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //MyFirstGeneratedClass1 asdf = new MyFirstGeneratedClass1();
+            Student raiman = new Student() { Name = "Raiman" };
+            Student hasan = new Student() { Name = "Hasan" };
+
+            Lecture kaw = new Lecture() { Name = "KAW" };
+
+            LectureGrade grade = new LectureGrade() { Name = "KAW-Note" };
+
+            Laptop laptop = new Laptop();
+        }
+    }
+}
