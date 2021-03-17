0b8c1769c9409549961f789c60d5e174dedbf332
kienboeck <kienboec@technikum-wien.at>
2021-03-17 16:17:30 +0100

    in class dev

---
+++ SETI.InClass/SETI.Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using SETI.Server;
+
+namespace SETI.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection();
+            connection.Start();
+            ISession session = connection.CreateSession();
+            IDestination dest = session.GetQueue("KAW.Seti");
+            IMessageConsumer consumer = session.CreateConsumer(dest);
+            IMessage message;
+            while ((message = consumer.Receive(TimeSpan.FromMilliseconds(2000))) != null)
+            {
+                var objectMessage = message as IObjectMessage;
+                var mapMessage = objectMessage?.Body as SETIMessage;
+                if (mapMessage?.Content?.Contains(">D") ?? false)
+                {
+                    Console.WriteLine(mapMessage.Content);
+                }
+                Thread.Sleep(TimeSpan.FromMinutes(1));
+            }
+
+            Console.WriteLine("... processing finished");
+        }
+    }
+}
---
+++ SETI.InClass/SETI.InClass/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+using SETI.Server;
+
+namespace SETI.InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+            IConnection connection = factory.CreateConnection();
+            connection.Start();
+            ISession session = connection.CreateSession();
+
+            IDestination dest = session.GetQueue("KAW.Seti");
+            IMessageProducer producer = session.CreateProducer(dest);
+
+            var lines = File.ReadAllLines("spacemap.txt");
+            foreach (var line in lines)
+            {
+                var objectMessage = producer.CreateObjectMessage(new SETIMessage() { Content = line });
+                producer.Send(objectMessage);
+            }
+
+            Console.WriteLine("... processing finished");
+        }
+    }
+}
